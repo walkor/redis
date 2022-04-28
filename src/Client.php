f28f9d71ad6cd7431e1974ade8d30bca98dec49a
walkor <walkor@users.noreply.github.com>
2022-04-28 11:23:54 +0800

    gc_collect_cycles

--- src/Client.php
+++ src/Client.php
@@ -394,6 +394,7 @@ class Client
             }
             if (empty($this->_queue)) {
                 $this->_queue = [];
+                gc_collect_cycles();
                 if (function_exists('gc_mem_caches')) {
                     gc_mem_caches();
                 }
